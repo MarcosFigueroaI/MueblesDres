466a66f57db4a96483df04cb44c2827d6dc5fad7
Marcos Figueroa <marcosfigueroaibarra@gmail.com>
2021-06-25 12:40:53 -0600

    Cambios por Android Update

--- app/src/main/java/com/marcosfigueroa/mueblesdres/MainViewModelFactory.kt
+++ app/src/main/java/com/marcosfigueroa/mueblesdres/MainViewModelFactory.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.marcosfigueroa.mueblesdres.repository.Repository
 
-class MainViewModelFactory(
-    private val repository: Repository
-    ): ViewModelProvider.Factory {
+class MainViewModelFactory(private val repository: Repository): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return MainViewModel(repository) as T
     }
